dce300b88df82f86490d470b01f3c046cdfee432
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-10 01:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let promise = new Promise((resolve,reject) => {
+	setTimeout(()=>{
+		resolve(`hello, world!`);
+	},1000)
+})
+
+promise.then((message)=> {
+	document.getElementById('output').innerText = message;
+})
+
+
